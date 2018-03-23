d582101d3e8642c5cfd8c524949bec7ed9ee51a3
ladada200 <misfitanimations@gmail.com>
2018-03-23 08:55:43 -0300

    Initial commit!
    Working after refactor

---
+++ Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace BinarySearch
+{
+    partial class frmBinSearch
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.nudNVal = new System.Windows.Forms.NumericUpDown();
+            this.nudXVal = new System.Windows.Forms.NumericUpDown();
+            this.lstBoxTree = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.nudNVal)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudXVal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(260, 13);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(103, 47);
+            this.btnSearch.TabIndex = 2;
+            this.btnSearch.Text = "Search Tree";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // nudNVal
+            // 
+            this.nudNVal.Location = new System.Drawing.Point(59, 13);
+            this.nudNVal.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.nudNVal.Name = "nudNVal";
+            this.nudNVal.Size = new System.Drawing.Size(195, 20);
+            this.nudNVal.TabIndex = 0;
+            // 
+            // nudXVal
+            // 
+            this.nudXVal.Location = new System.Drawing.Point(59, 40);
+            this.nudXVal.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.nudXVal.Name = "nudXVal";
+            this.nudXVal.Size = new System.Drawing.Size(195, 20);
+            this.nudXVal.TabIndex = 1;
+            // 
+            // lstBoxTree
+            // 
+            this.lstBoxTree.FormattingEnabled = true;
+            this.lstBoxTree.Location = new System.Drawing.Point(13, 67);
+            this.lstBoxTree.Name = "lstBoxTree";
+            this.lstBoxTree.Size = new System.Drawing.Size(602, 225);
+            this.lstBoxTree.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(16, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Height:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Value:";
+            // 
+            // frmBinSearch
+            // 
+            this.AcceptButton = this.btnSearch;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 311);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstBoxTree);
+            this.Controls.Add(this.nudXVal);
+            this.Controls.Add(this.nudNVal);
+            this.Controls.Add(this.btnSearch);
+            this.Name = "frmBinSearch";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Binary Search | Christopher Rupert";
+            this.Load += new System.EventHandler(this.frmBinSearch_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.nudNVal)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudXVal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.NumericUpDown nudNVal;
+        private System.Windows.Forms.NumericUpDown nudXVal;
+        private System.Windows.Forms.ListBox lstBoxTree;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinarySearch
+{
+    public partial class frmBinSearch : Form
+    {
+        public frmBinSearch()
+        {
+            InitializeComponent();
+        }
+
+        private void frmBinSearch_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            //Predefine the values of the Numeric up-downs.
+            int NVal = (int)nudNVal.Value; int XVal = (int)nudXVal.Value;
+            //Now we'll discern the following:
+            
+            functionGet(NVal, XVal);
+
+            /*
+             * Set LOW to 1 and HIGH to N - 1;
+             * IF LOW is greater than HIGH break;
+             * Otherwise; set MID to LOW + HIGH divided by two.
+             * if X (input) is greater than the mid change LOW to mid + 1;
+             * However, if X (input) is less than the mid, change the HIGH to mid - 1;
+             * 
+             * By reducing the field size, we're able to find the near exact positioning.
+             * 
+             * m = mid;
+             * L = LOW;
+             * R = HIGH;
+             * 
+             * For sanity checks, evalute that loop must be broken if the LOWer end of the scale-
+             * is not less than or equal to the HIGHer end.
+             */
+        }
+        private void functionGet(int N, int X)
+        {
+            int LOW = 1;
+            int HIGH = N -1;
+            int mid = 0;
+
+            //Start by clearing the list box.
+            lstBoxTree.Items.Clear();
+
+            //First output;
+            lstBoxTree.Items.Add(LOW.ToString() + " " + (HIGH + 1).ToString());
+
+            //Start by creating two lists; one for tight discovery in binary search;
+            //The other for confirmation that number exists in a given array.
+
+            List<int> nums = new List<int>();
+            List<int> oNums = new List<int>();
+            for (int i = LOW; i <= HIGH; i++)
+            {
+                nums.Add(i);
+            }//End for
+
+            for (int i = LOW; i <= HIGH + 1; i++)
+            {
+                oNums.Add(i);
+            }//End for
+
+            //Conditionally; if the input value of X (from user input) is higher than the ceiling (N - 1)
+            //This evalutes to true; which simply means that the user is trying to search out of range.
+            if (X > HIGH)
+            {
+                MessageBox.Show("Error, Ceiling too low");
+            }  else
+            {
+                
+                //I struggled with precision here, until I realised that less-than-equal-to was necessary.
+                //It should be noted here that without the equals to the loop will end abruptly with-
+                //A list of 11 integers if given an X value of 37 (this is because 37 would be the mid-point
+                while (LOW <= HIGH) 
+                {
+                    //Exchange a variable MID to be the ceiling and the floor together; then divide by two to begin branching
+                    mid = (HIGH + LOW) / 2;
+                    if (mid < X)
+                    {
+                        LOW = mid + 1;
+                    }
+                    else
+                    {
+                        HIGH = mid - 1;
+                    }
+                    
+                    //Clear the smaller array; we'll be able to compare array sizes much later if necessary.
+                    nums.Clear();
+                    //Re-establish array nums by adding integers to array (This is another sanity check for further comparison)
+                    for (int i = LOW; i < HIGH; i++)
+                    {
+                        nums.Add(i);
+                    }
+
+                    //Output floor and ceiling to list-box for quick breakdown.
+                    if (LOW == HIGH +1)
+                    {
+                        //Unless we're on the final string output
+                        lstBoxTree.Items.Add(LOW.ToString());
+                    } else
+                    {
+                        lstBoxTree.Items.Add(LOW.ToString() + " " + (HIGH + 1).ToString());
+                    }
+                    
+                   
+                }
+                    //Output where the number can be expected to be found in the array oNums.
+                    lstBoxTree.Items.Add("Found " + mid.ToString() + " At Pos, " + oNums[mid].ToString());
+            }
+        }
+    }
+}
